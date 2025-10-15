826bfea75fe366c2439cf0aa77361d16fa6fb047
Muaaz Shoaib <muaazshoaib1670@gmail.com>
2025-10-15 09:01:55 +0500

    262. Core Syntax Refresher

---
+++ javascript_refresher/play.js
@@ -0,0 +1,18 @@
+var name = "Max";
+var age = 29;
+var hasHobbies = true;
+
+// console.log(name);
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies " +
+    userHasHobby
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
