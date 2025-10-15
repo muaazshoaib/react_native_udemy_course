07927b78e05417e48a7b3004b9f0456de5155360
muaazshoaib <muaazshoaib1670@gmail.com>
2025-10-15 12:02:54 +0500

    270. Async Code & Promises

--- javascript_refresher/play.js
+++ javascript_refresher/play.js
@@ -1,53 +1,23 @@
-const person = {
-  name: "Max",
-  age: 29,
-  greet() {
-    console.log("Hi, I am " + this.name);
-  },
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, 1500);
+  });
+  return promise;
 };
 
-// const printName = (personData) => {
-//   console.log(personData.name);
-// };
+setTimeout(() => {
+  console.log("Timer is done!");
+  fetchData()
+    .then((text) => {
+      console.log(text);
+      return fetchData();
+    })
+    .then((text2) => {
+      console.log(text2);
+    });
+}, 2000);
 
-const printName = ({ name }) => {
-  console.log(name);
-};
-
-printName(person);
-
-const { name, age } = person;
-console.log(name, age);
-
-// const copiedPerson = { ...person };
-// console.log(copiedPerson);
-
-hobbies = ["Sports", "Cooking"];
-const [hobby1, hobby2] = hobbies;
-console.log(hobby1, hobby2);
-
-// // for (let hobby of hobbies) {
-// //   console.log(hobby);
-// // }
-
-// // console.log(hobbies.map((hobby) => "Hobby : " + hobby));
-// // console.log(hobbies);
-
-// // hobbies.push("Programming");
-// // console.log(hobbies);
-
-// // const copiedArray = hobbies.slice();
-// // const copiedArray = [hobbies];
-// const copiedArray = [...hobbies];
-// console.log(copiedArray);
-
-// // const toArray = (arg1, arg2, arg3) => {
-// //     return [arg1, arg2, arg3];
-// // };
-
-// const toArray = (...args) => {
-//   return args;
-// };
-
-// console.log(toArray(1, 2, 3));
-// console.log(toArray(1, 2, 3, 4));
+console.log("Hello!");
+console.log("Hi!");
