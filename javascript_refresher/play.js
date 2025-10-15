4fed3235c01f453743a815c7c7c203ed49be0878
muaazshoaib <muaazshoaib1670@gmail.com>
2025-10-15 10:49:14 +0500

    265. Objects: Properties & Methods

--- javascript_refresher/play.js
+++ javascript_refresher/play.js
@@ -1,24 +1,10 @@
-var name = "Max";
-var age = 29;
-var hasHobbies = true;
-
-const summarizeUser = (userName, userAge, userHasHobby) => {
-  return (
-    "Name is " +
-    userName +
-    ", age is " +
-    userAge +
-    " and the user has hobbies " +
-    userHasHobby
-  );
+const person = {
+  name: "Max",
+  age: 29,
+  greet() {
+    console.log("Hi, I am " + this.name);
+  },
 };
 
-// const add = (a, b) => a + b;
-const addOne = (a) => a + 1;
-const addRandom = () => 1 + 2;
-
-// console.log(add(1, 2));
-// console.log(addOne(1));
-console.log(addRandom());
-
-console.log(summarizeUser(name, age, hasHobbies));
+person.greet();
+console.log(person);
