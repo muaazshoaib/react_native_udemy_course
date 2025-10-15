7f7b6d2fe2009c9cf82c9a737db0f15eefb3ef81
muaazshoaib <muaazshoaib1670@gmail.com>
2025-10-15 11:23:21 +0500

    268. Spread Operator & Rest Parameters

--- javascript_refresher/play.js
+++ javascript_refresher/play.js
@@ -6,6 +6,9 @@ const person = {
   },
 };
 
+const copiedPerson = { ...person };
+console.log(copiedPerson);
+
 hobbies = ["Sports", "Cooking"];
 
 // for (let hobby of hobbies) {
@@ -15,5 +18,21 @@ hobbies = ["Sports", "Cooking"];
 // console.log(hobbies.map((hobby) => "Hobby : " + hobby));
 // console.log(hobbies);
 
-hobbies.push("Programming");
-console.log(hobbies);
+// hobbies.push("Programming");
+// console.log(hobbies);
+
+// const copiedArray = hobbies.slice();
+// const copiedArray = [hobbies];
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+// const toArray = (arg1, arg2, arg3) => {
+//     return [arg1, arg2, arg3];
+// };
+
+const toArray = (...args) => {
+  return args;
+};
+
+console.log(toArray(1, 2, 3));
+console.log(toArray(1, 2, 3, 4));
